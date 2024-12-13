07cdcb5168c3ba22b41f762cebb3eeaaa4ca1867
Pranavbc <pranav1952000@gmail.com>
2024-12-13 15:12:56 +0530

    url

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://videotube-lovat.vercel.app/";
+export const BASE_URL = "https://videotube-lovat.vercel.app";
 
 // export const BASE_URL = import.meta.env.VITE_BACKEND_URL || import.meta.env.BACKEND_URL;
